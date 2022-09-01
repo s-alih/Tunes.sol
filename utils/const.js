96844abda1f7670c702dcb48beeb8dff63c9f398
Salih Poovathingal Hussainar <salihpoovathingalhussainar@Salihs-MacBook-Air.local>
2022-09-01 13:53:26 +0530

    solana interface

--- utils/const.js
+++ utils/const.js
@@ -1,6 +1,10 @@
 import { clusterApiUrl, PublicKey } from "@solana/web3.js";
+import spotify from "./spotify.json";
+
 export const SOLANA_HOST = clusterApiUrl("devnet");
 
 export const STABLE_POOL_PROGRAM_ID = new PublicKey(
-  "5wSMdEYxSW7iB3rdE7c8yB3bqBUrVtvggjDdY5viyDLk"
+  "A4yw6wtFgaPKCQv5X3F9V3oY8cscB6fVtPjdjskU97Nx"
 );
+
+export const STABLE_POOL_IDL = spotify;
